3777c7b185d8b406abfbbf56ad35b462ecbaeb6a
yu-gi-oh-leilei <106001396+yu-gi-oh-leilei@users.noreply.github.com>
2022-10-06 14:45:19 +0800

    add utils

---
+++ utils/utils.py
@@ -0,0 +1,297 @@
+import os, sys, pdb
+import math
+import torch
+from PIL import Image
+import numpy as np
+import random
+import torch.nn as nn
+from copy import deepcopy
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+    
+    def average(self):
+        return self.avg
+    
+    def value(self):
+        return self.val
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+class AveragePrecisionMeter(object):
+    """
+    The APMeter measures the average precision per class.
+    The APMeter is designed to operate on `NxK` Tensors `output` and
+    `target`, and optionally a `Nx1` Tensor weight where (1) the `output`
+    contains model output scores for `N` examples and `K` classes that ought to
+    be higher when the model is more convinced that the example should be
+    positively labeled, and smaller when the model believes the example should
+    be negatively labeled (for instance, the output of a sigmoid function); (2)
+    the `target` contains only values 0 (for negative examples) and 1
+    (for positive examples); and (3) the `weight` ( > 0) represents weight for
+    each sample.
+    """
+
+    def __init__(self, difficult_examples=True):
+        super(AveragePrecisionMeter, self).__init__()
+        self.reset()
+        self.difficult_examples = difficult_examples
+
+    def reset(self):
+        """Resets the meter with empty member variables"""
+        self.scores = torch.FloatTensor(torch.FloatStorage())
+        self.targets = torch.LongTensor(torch.LongStorage())
+        self.filenames = []
+
+    def add(self, output, target, filename):
+        """
+        Args:
+            output (Tensor): NxK tensor that for each of the N examples
+                indicates the probability of the example belonging to each of
+                the K classes, according to the model. The probabilities should
+                sum to one over all classes
+            target (Tensor): binary NxK tensort that encodes which of the K
+                classes are associated with the N-th input
+                    (eg: a row [0, 1, 0, 1] indicates that the example is
+                         associated with classes 2 and 4)
+            weight (optional, Tensor): Nx1 tensor representing the weight for
+                each example (each weight > 0)
+        """
+        if not torch.is_tensor(output):
+            output = torch.from_numpy(output)
+        if not torch.is_tensor(target):
+            target = torch.from_numpy(target)
+
+        if output.dim() == 1:
+            output = output.view(-1, 1)
+        else:
+            assert output.dim() == 2, \
+                'wrong output size (should be 1D or 2D with one column \
+                per class)'
+        if target.dim() == 1:
+            target = target.view(-1, 1)
+        else:
+            assert target.dim() == 2, \
+                'wrong target size (should be 1D or 2D with one column \
+                per class)'
+        if self.scores.numel() > 0:
+            assert target.size(1) == self.targets.size(1), \
+                'dimensions for output should match previously added examples.'
+
+        # make sure storage is of sufficient size
+        if self.scores.storage().size() < self.scores.numel() + output.numel():
+            new_size = math.ceil(self.scores.storage().size() * 1.5)
+            self.scores.storage().resize_(int(new_size + output.numel()))
+            self.targets.storage().resize_(int(new_size + output.numel()))
+
+        # store scores and targets
+        offset = self.scores.size(0) if self.scores.dim() > 0 else 0
+        self.scores.resize_(offset + output.size(0), output.size(1))
+        self.targets.resize_(offset + target.size(0), target.size(1))
+        self.scores.narrow(0, offset, output.size(0)).copy_(output)
+        self.targets.narrow(0, offset, target.size(0)).copy_(target)
+
+        self.filenames += filename # record filenames
+
+    def value(self):
+        """Returns the model's average precision for each class
+        Return:
+            ap (FloatTensor): 1xK tensor, with avg precision for each class k
+        """
+
+        if self.scores.numel() == 0:
+            return 0
+        ap = torch.zeros(self.scores.size(1))
+        rg = torch.arange(1, self.scores.size(0)).float()
+        # compute average precision for each class
+        for k in range(self.scores.size(1)):
+            # sort scores
+
+            scores = self.scores[:, k]
+            targets = self.targets[:, k]
+            
+            # compute average precision
+            ap[k] = AveragePrecisionMeter.average_precision(scores, targets, self.difficult_examples)
+        return ap
+
+    @staticmethod
+    def average_precision(output, target, difficult_examples=True):
+
+        # sort examples
+        sorted, indices = torch.sort(output, dim=0, descending=True)
+
+        # Computes prec@i
+        pos_count = 0.
+        total_count = 0.
+        precision_at_i = 0.
+        for i in indices:
+            label = target[i]
+            if difficult_examples and label == 0:
+                continue
+            if label == 1:
+                pos_count += 1
+            total_count += 1
+            if label == 1:
+                precision_at_i += pos_count / total_count
+        precision_at_i /= pos_count
+        return precision_at_i
+
+    def overall(self):
+        if self.scores.numel() == 0:
+            return 0
+        scores = self.scores.cpu().numpy()
+        targets = self.targets.clone().cpu().numpy()
+        targets[targets == -1] = 0
+        return self.evaluation(scores, targets)
+
+    def overall_topk(self, k):
+        targets = self.targets.clone().cpu().numpy()
+        targets[targets == -1] = 0
+        n, c = self.scores.size()
+        scores = np.zeros((n, c)) - 1
+        index = self.scores.topk(k, 1, True, True)[1].cpu().numpy()
+        tmp = self.scores.cpu().numpy()
+        for i in range(n):
+            for ind in index[i]:
+                scores[i, ind] = 1 if tmp[i, ind] >= 0 else -1
+        return self.evaluation(scores, targets)
+
+    def evaluation(self, scores_, targets_):
+        n, n_class = scores_.shape
+        Nc, Np, Ng = np.zeros(n_class), np.zeros(n_class), np.zeros(n_class)
+        for k in range(n_class):
+            scores = scores_[:, k]
+            targets = targets_[:, k]
+            targets[targets == -1] = 0
+            Ng[k] = np.sum(targets == 1)
+            Np[k] = np.sum(scores >= 0)
+            Nc[k] = np.sum(targets * (scores >= 0))
+        Np[Np == 0] = 1
+        OP = np.sum(Nc) / np.sum(Np)
+        OR = np.sum(Nc) / np.sum(Ng)
+        OF1 = (2 * OP * OR) / (OP + OR)
+
+        CP = np.sum(Nc / Np) / n_class
+        CR = np.sum(Nc / Ng) / n_class
+        CF1 = (2 * CP * CR) / (CP + CR)
+        return OP, OR, OF1, CP, CR, CF1
+
+
+class WarmUpLR(torch.optim.lr_scheduler._LRScheduler):
+    def __init__(self, optimizer, total_iters, last_epoch=-1):
+        self.total_iters = total_iters
+        super().__init__(optimizer, last_epoch=last_epoch)
+
+    def get_lr(self):
+        return [base_lr * self.last_epoch / (self.total_iters + 1e-8) for base_lr in self.base_lrs]
+
+
+
+class AsymmetricLossOptimized(nn.Module):
+    ''' Notice - optimized version, minimizes memory allocation and gpu uploading,
+    favors inplace operations'''
+
+    def __init__(self, gamma_neg=4, gamma_pos=1, clip=0.05, eps=1e-5, disable_torch_grad_focal_loss=False):
+        super(AsymmetricLossOptimized, self).__init__()
+
+        self.gamma_neg = gamma_neg
+        self.gamma_pos = gamma_pos
+        self.clip = clip
+        self.disable_torch_grad_focal_loss = disable_torch_grad_focal_loss
+        self.eps = eps
+
+        self.targets = self.anti_targets = self.xs_pos = self.xs_neg = self.asymmetric_w = self.loss = None
+
+    def forward(self, x, y):
+        """"
+        Parameters
+        ----------
+        x: input logits
+        y: targets (multi-label binarized vector)
+        """
+        # import ipdb; ipdb.set_trace()
+
+        self.targets = y
+        self.anti_targets = 1 - y
+
+        # Calculating Probabilities
+        self.xs_pos = torch.sigmoid(x)
+        self.xs_neg = 1.0 - self.xs_pos
+
+        # Asymmetric Clipping
+        if self.clip is not None and self.clip > 0:
+            self.xs_neg.add_(self.clip).clamp_(max=1)
+
+        # Basic CE calculation
+        self.loss = self.targets * torch.log(self.xs_pos.clamp(min=self.eps))
+        self.loss.add_(self.anti_targets * torch.log(self.xs_neg.clamp(min=self.eps)))
+
+        # Asymmetric Focusing
+        if self.gamma_neg > 0 or self.gamma_pos > 0:
+            if self.disable_torch_grad_focal_loss:
+                with torch.no_grad():
+                    if self.disable_torch_grad_focal_loss:
+                        torch.set_grad_enabled(False)
+                    self.xs_pos = self.xs_pos * self.targets
+                    self.xs_neg = self.xs_neg * self.anti_targets
+                    self.asymmetric_w = torch.pow(1 - self.xs_pos - self.xs_neg,
+                                                self.gamma_pos * self.targets + self.gamma_neg * self.anti_targets)
+                    if self.disable_torch_grad_focal_loss:
+                        torch.set_grad_enabled(True)
+                self.loss *= self.asymmetric_w
+            else:
+                self.xs_pos = self.xs_pos * self.targets
+                self.xs_neg = self.xs_neg * self.anti_targets
+                self.asymmetric_w = torch.pow(1 - self.xs_pos - self.xs_neg,
+                                            self.gamma_pos * self.targets + self.gamma_neg * self.anti_targets)   
+                self.loss *= self.asymmetric_w         
+        _loss = - self.loss.sum() / x.size(0)
+        _loss = _loss / y.size(1) * 1000
+
+        return _loss
+
+
+class ModelEma(torch.nn.Module):
+    def __init__(self, model, decay=0.9997, device=None):
+        super(ModelEma, self).__init__()
+        # make a copy of the model for accumulating moving average of weights
+        self.module = deepcopy(model)
+        self.module.eval()
+
+        # import ipdb; ipdb.set_trace()
+
+        self.decay = decay
+        self.device = device  # perform ema on different device from model if set
+        if self.device is not None:
+            self.module.to(device=device)
+
+    def _update(self, model, update_fn):
+        with torch.no_grad():
+            for ema_v, model_v in zip(self.module.state_dict().values(), model.state_dict().values()):
+                if self.device is not None:
+                    model_v = model_v.to(device=self.device)
+                ema_v.copy_(update_fn(ema_v, model_v))
+
+    def update(self, model):
+        self._update(model, update_fn=lambda e, m: self.decay * e + (1. - self.decay) * m)
+
+    def set(self, model):
+        self._update(model, update_fn=lambda e, m: m)
